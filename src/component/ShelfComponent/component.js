8ec7775ecee90eb59b0708e84099247a32292433
jasdeep06 <pssaluja1992@gmail.com>
2017-11-12 12:52:28 +0530

    changes

--- src/component/ShelfComponent/component.js
+++ src/component/ShelfComponent/component.js
@@ -31,7 +31,7 @@ export default class BookShelf extends React.Component {
             <div className="book-top">
               <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail: ''})` }}></div>
               <div className="book-shelf-changer">
-                <select defaultValue={book.shelf} onChange={this.handleChange.bind(this,book)}>
+                <select value={book.shelf} onChange={this.handleChange.bind(this,book)}>
                   <option value="none" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead" >Want to Read</option>
